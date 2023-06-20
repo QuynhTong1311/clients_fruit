b2a9193b2c4b0206ac23ba1e73e08536a5e29a95
Quốc Dũng <91933007+QuocDung21@users.noreply.github.com>
2023-06-20 14:17:17 +0700

    add

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -19,20 +19,34 @@ class MyDocument extends Document {
             property="og:description"
             content="React Store e-commerce Template"
           />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-          <script>{`
-          var chatbox = document.getElementById('fb-customer-chat');
-          chatbox.setAttribute("page_id", "107771938626773");
-          chatbox.setAttribute("attribution", "biz_inbox");
-        `}</script>
+          <script>
+            {`
+                var chatbox = document.getElementById('fb-customer-chat');
+                chatbox.setAttribute("page_id", "102259689585232");
+                chatbox.setAttribute("attribution", "biz_inbox");
+             `}
+          </script>
           <script
             src="https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js"
             async
             defer
           ></script>
+        </Head>
+        <body>
+          <script>
+            {`
+                var chatbox = document.getElementById('fb-customer-chat');
+                chatbox.setAttribute("page_id", "102259689585232");
+                chatbox.setAttribute("attribution", "biz_inbox");
+             `}
+          </script>
+          <script
+            src="https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js"
+            async
+            defer
+          ></script>
+          <Main />
+          <NextScript />
         </body>
       </Html>
     );
